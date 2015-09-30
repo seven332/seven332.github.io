c9a5164aa4f78060d40236a1e50fede2d9314391
seven332 <seven332@163.com>
2015-10-01 00:09:33 +0800

    Add rss

--- pelicanconf.py
+++ pelicanconf.py
@@ -26,17 +26,25 @@ PLUGINS = ('minify',)
 DISQUS_SITENAME = "seven332"
 
 # Feed generation is usually not desired when developing
+FEED_RSS = None
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_RSS = None
+AUTHOR_FEED_RSS = None
+TAG_FEED_RSS = None
+TRANSLATION_FEED_RSS = None
+
+FEED_ATOM = None
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
+TAG_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
 
 # Blogroll
 LINKS = ((u'无', '#'),)
 
 # Social widget
-SOCIAL = ((u'无', '#'),)
+SOCIAL = (('github', 'http://github.com/seven332'),)
 
 DEFAULT_PAGINATION = False
 
